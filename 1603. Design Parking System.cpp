d5494f0755b99be85e1c4f1f56ea6b304b795b6a
Nirmal Kumar Gupta <92977085+The-ZeGOD@users.noreply.github.com>
2023-06-05 12:12:12 +0530

    Create 1603. Design Parking System.cpp

---
+++ 1603. Design Parking System.cpp
@@ -0,0 +1,16 @@
+class ParkingSystem {
+
+public:
+vector<int> counts;
+    ParkingSystem(int big, int medium, int small) {
+        counts = {big, medium, small};
+    }
+    
+    bool addCar(int carType) {
+        if(counts[carType-1] > 0){
+                        --counts[carType-1];
+                                    return true;
+        }
+        return false;
+    }
+};
